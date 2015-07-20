cc9f6831870381c6eb15cc68b956561d42bf2887
egoisticum <almir.dodigovic@gmail.com>
2015-07-20 17:38:25 +0200

    first commit

---
+++ js/helpers.js
@@ -0,0 +1,35 @@
+var JQ = jQuery.noConflict();
+
+var ambitiousSlider = {
+  initSlider: function(elem){
+    var childrenItems = JQ(elem + ".inner").children(".item").length;//get all slider items count
+    var itemWidth;//set width of any of item
+    var itemHeight;
+
+    //ok let's make it full responsive
+    switch(true){
+      case(JQ(window).width() < 400): itemWidth = JQ(elem).width() * 0.85; break;
+      case(JQ(window).width() >= 400 && JQ(window).width() < 600): itemWidth = JQ(elem).width() * 0.65; break;
+      case(JQ(window).width() >= 600 && JQ(window).width() < 1000): itemWidth = JQ(elem).width() * 0.37; break;
+      default: itemWidth = JQ(elem).width() * 0.25; break;
+    }
+    var innerWidth = itemWidth * (childrenItems+1);//set width of .inner div inside slider
+    //set css width of inner and of any specific item
+    JQ(elem + '.inner').css("width", innerWidth + "px");
+    JQ(elem + '.inner .item').css("width", itemWidth + "px");
+    //set css height of slider
+    itemHeight = JQ(elem + '.inner').height() + 50;
+    JQ(elem).css("height", itemHeight + "px");
+  },
+  left: function(elem){
+    JQ(elem + '.inner .item').last().prependTo('.slider .inner');
+    JQ(elem + '.inner .item').addClass('fadeIn');
+  },
+  right: function(elem){
+    JQ(elem + '.inner .item').first().appendTo('.slider .inner');
+    JQ(elem + '.inner .item').addClass('fadeIn');
+  },
+  specific: function(elem, index){
+    JQ()
+  }
+};
---
+++ js/script.js
@@ -0,0 +1,57 @@
+/*
+Name: script.js
+Description: execution javaScript code
+Author: Almir Dodigovic
+Collaborators: Haris Hadziahmetovic
+*/
+var jq = jQuery.noConflict();
+jq(document).ready(function(){
+	//on scroll hide top-header bar
+	jq(window).on("load scroll resize", function(){
+		var elem = jq("#mainHeader .topHeader");
+		if(jq(elem).visible() && jq(window).innerWidth() >= 768){
+			jq("#mainHeader #mainMenu").css("top", "40px");
+		}
+		else{
+			jq("#mainHeader #mainMenu").css("top", "0");
+		}
+	});
+	//on scrol and on load hide remove "container-fluid class" from menu
+	function fullWidthMobileMenu(){
+		if(jq(window).innerWidth() <= 768){
+			jq("#mainMenu .container-fluid").removeClass("container-fluid");
+		}
+		else{
+			jq("#mainMenu .container-fluid").addClass("container-fluid");
+		}
+	}
+	fullWidthMobileMenu();
+	jq(window).resize(function(){
+		fullWidthMobileMenu();
+	});
+	//navbar changing toggle button icons
+	function toggleNavbarButton(){
+		jq("#mainHeader #mainMenu .navbar-toggle").click(function(){
+			jq("#mainHeader #mainMenu .navbar-toggle i.menu-icon").toggleClass("close");
+			jq("#mainHeader #mainMenu .navbar-header").toggleClass("collapsed");
+		});
+	}
+	toggleNavbarButton();
+
+	//slider
+	var sliderCourses = "#courses-slider ";
+	JQ(window).load(function(){
+		ambitiousSlider.initSlider(sliderCourses);
+	});
+	JQ(window).resize(function(){
+		ambitiousSlider.initSlider(sliderCourses);
+	});
+	//left-click
+	JQ(sliderCourses + '#left-nav').click(function (e) {
+		ambitiousSlider.left(sliderCourses);
+	});
+	//prev-click
+	JQ(sliderCourses + '#right-nav').click(function (e) {
+		ambitiousSlider.right(sliderCourses);
+	});
+});
