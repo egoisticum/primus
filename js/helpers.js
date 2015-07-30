ef7bf7837074866ac352221ce5bc4179dc22188e
egoisticum <almir.dodigovic@gmail.com>
2015-07-30 22:58:06 +0200

    twiking main courses slider

--- js/helpers.js
+++ js/helpers.js
@@ -8,10 +8,10 @@ var ambitiousSlider = {
 
     //ok let's make it full responsive
     switch(true){
-      case(JQ(window).width() < 400): itemWidth = JQ(elem).width() * 0.85; break;
-      case(JQ(window).width() >= 400 && JQ(window).width() < 600): itemWidth = JQ(elem).width() * 0.65; break;
-      case(JQ(window).width() >= 600 && JQ(window).width() < 1000): itemWidth = JQ(elem).width() * 0.37; break;
-      default: itemWidth = JQ(elem).width() * 0.25; break;
+      case(JQ(window).width() < 400): itemWidth = JQ(window).width() * 0.85; break;
+      case(JQ(window).width() >= 400 && JQ(window).width() < 600): itemWidth = JQ(window).width() * 0.65; break;
+      case(JQ(window).width() >= 600 && JQ(window).width() < 1000): itemWidth = JQ(window).width() * 0.37; break;
+      default: itemWidth = JQ(window).width() * 0.20; break;
     }
     var innerWidth = itemWidth * (childrenItems+1);//set width of .inner div inside slider
     //set css width of inner and of any specific item
@@ -19,7 +19,8 @@ var ambitiousSlider = {
     JQ(elem + '.inner .item').css("width", itemWidth + "px");
     //set css height of slider
     itemHeight = JQ(elem + '.inner').height() + 50;
-    JQ(elem).css("height", itemHeight + "px");
+    //JQ(elem).css("height", itemHeight + "px");
+    JQ(elem).css("height", "630px");
   },
   left: function(elem){
     JQ(elem + '.inner .item').last().prependTo(elem + '.inner');
